f32a20f968f4a40cc491fe68d85eda9f08ee4a75
张红亮 <zhanghongliang@gaiay.cn>
2017-08-16 14:13:58 +0800

    + add mytest

---
+++ plugins/mytest/mytest.plugin.zsh
@@ -0,0 +1,25 @@
+#cpv() {
+#    rsync -pogbr -hhh --backup-dir=/tmp/rsync -e /dev/null --progress "$@"
+#}
+#compdef _files cpv
+#
+#
+mytest(){
+    echo "`date` $@"
+}
+
+cc=""
+function listMytestComplections {
+   echo "$cc "
+   if [ -n $cc ];then
+        cc="`date` 123"
+        echo "`date`"
+   fi
+   reply=(
+        abc abc:zhl abc
+        test test:zhl testCompectrl
+    );
+}
+
+#compctl -K listMytestComplections mytest
+compctl -K listMytestComplections mytest
